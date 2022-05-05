4b149b9faf49d59ccaae846559a28715e8dd90ef
Yck61 <yakupck1967@gmail.com>
2022-05-05 23:23:50 +0300

    fourth commit

--- src/test/java/com/practice/yakup/YakupJava.java
+++ src/test/java/com/practice/yakup/YakupJava.java
@@ -4,13 +4,11 @@ public class YakupJava {
 
 	public static void main(String[] args) {
 
-
-		for (int i = 0; i < 10; i++) {
-			System.out.println(i);
+		for (int i = 0; i < 15; i++) {
+			System.out.println(i + "merge conflict is gonna happen");
 		}
 
 		System.out.println("ready");
-
 
 	}
 
