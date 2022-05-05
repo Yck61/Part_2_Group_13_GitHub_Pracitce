972ccbc575a37c814816f4975351615aaa0e7974
Ahmet-Yurtsever <103644701+Ahmet-Yurtsever@users.noreply.github.com>
2022-05-05 22:52:41 +0300

    new add

--- src/test/java/com/practice/Ahmet/AhmetFirstJava.java
+++ src/test/java/com/practice/Ahmet/AhmetFirstJava.java
@@ -3,7 +3,10 @@ package com.practice.Ahmet;
 public class AhmetFirstJava {
 
     public static void main(String[] args) {
-
         System.out.println("Hello Group13");
+
+        for (int i=0; i < 8; i++){
+            System.out.println("bye bye");
+        }
     }
 }
