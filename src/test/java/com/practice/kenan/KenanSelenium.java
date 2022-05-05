d608bca110bce12b24fc7fa1968873a8bc5a912b
mkenan1 <mkenanoglu@hotmail.com>
2022-05-05 23:11:35 +0300

    Coding

--- src/test/java/com/practice/kenan/KenanSelenium.java
+++ src/test/java/com/practice/kenan/KenanSelenium.java
@@ -5,7 +5,7 @@ public class KenanSelenium {
 
         System.out.println("Eid Mubarek");
         System.out.println("How is it going on?");
-        System.out.println("Are you ready for coding?");
+        System.out.println("Are u ready for coding?");
 
 
 
