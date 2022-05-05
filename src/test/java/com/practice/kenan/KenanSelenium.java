9b7c898e42eaf7169c9eb046627bc024a2d5fddd
mkenan1 <mkenanoglu@hotmail.com>
2022-05-05 23:13:32 +0300

    New pull is coming

--- src/test/java/com/practice/kenan/KenanSelenium.java
+++ src/test/java/com/practice/kenan/KenanSelenium.java
@@ -6,6 +6,7 @@ public class KenanSelenium {
         System.out.println("Eid Mubarek");
         System.out.println("How is it going on?");
         System.out.println("Are u ready for coding?");
+        System.out.println("What is the problem");
 
 
 
