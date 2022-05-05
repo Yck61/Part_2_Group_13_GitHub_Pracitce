667654dbfe86bc31bd88d2f3330c40c026e9eb43
Muhammed Ali Çelik <muhammedalicelikk@gmail.com>
2022-05-05 22:50:19 +0300

    third

--- src/test/java/com/practice/mali/mali.java
+++ src/test/java/com/practice/mali/mali.java
@@ -5,6 +5,7 @@ public class mali {
     public static void main(String[] args) {
         System.out.println("s.a");
         System.out.println("2022");
+        System.out.println("Hello");
     }
 
 }
